3fa1361304b963b5499d25db54aa8cdbb25fb602
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-05-10 18:26:47 -0300

    fix

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,8 +1,9 @@
-
 /* Dark Mode */
 
 const html = document.querySelector('html');
 const mode = document.querySelector('#mode-icon');
+const mailD = document.querySelector('#mail-dark')
+const mailL = document.querySelector('#mail-light')
 
 mode.addEventListener('click', function(){
 html.classList.toggle('light-mode');
@@ -12,12 +13,18 @@ mode.addEventListener('click', () => {
     if(mode.classList.contains('fa-moon')){
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
+        mailL.style.display="none"
+        mailD.style.display="inline"
+
+
 
         return;
     }
         mode.classList.add('fa-moon');
         mode.classList.remove('fa-sun');
-    }
+        mailD.style.display="none"
+        mailL.style.display="inline"
+    }   
 );
 
 
