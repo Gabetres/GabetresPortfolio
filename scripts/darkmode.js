c275c3a57e3396046cc4700829b998cc3bb9b944
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-05-25 17:18:26 -0300

    update

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -6,6 +6,8 @@ const mailD = document.querySelector('#mail-dark')
 const mailL = document.querySelector('#mail-light')
 const gabrielD = document.querySelector('#gabriel-dark')
 const gabrielL = document.querySelector('#gabriel-light')
+const notebookL = document.querySelector('#notebook-light')
+const notebookD = document.querySelector('#notebook-dark')
 
 mode.addEventListener('click', function(){
 html.classList.toggle('light-mode');
@@ -19,6 +21,8 @@ mode.addEventListener('click', () => {
         mailL.classList.toggle('d-lg-inline');
         gabrielD.classList.toggle('d-sm-none');
         gabrielL.classList.toggle('d-sm-inline');
+        notebookD.classList.toggle('d-md-none');
+        notebookL.classList.toggle('d-md-inline');
 
         return;
     }
@@ -28,6 +32,8 @@ mode.addEventListener('click', () => {
         mailL.classList.toggle('d-lg-inline');
         gabrielD.classList.toggle('d-sm-none');
         gabrielL.classList.toggle('d-sm-inline');
+        notebookD.classList.toggle('d-md-none');
+        notebookL.classList.toggle('d-md-inline');
     }   
 );
 
