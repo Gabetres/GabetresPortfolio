57716c8e0b44acddfa97e981691f20ffdfc0375a
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-06-23 18:27:00 -0300

    Add files via upload

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -2,38 +2,38 @@
 
 const html = document.querySelector('html');
 const mode = document.querySelector('#mode-icon');
-const mailD = document.querySelector('#mail-dark')
 const mailL = document.querySelector('#mail-light')
-const gabrielD = document.querySelector('#gabriel-dark')
+const mailD = document.querySelector('#mail-dark')
 const gabrielL = document.querySelector('#gabriel-light')
-const notebookL = document.querySelector('#notebook-light')
+const gabrielD = document.querySelector('#gabriel-dark')
 const notebookD = document.querySelector('#notebook-dark')
+const notebookL = document.querySelector('#notebook-light')
 
 mode.addEventListener('click', function(){
-html.classList.toggle('light-mode');
+html.classList.toggle('dark-mode');
 });
 
 mode.addEventListener('click', () => {
     if(mode.classList.contains('fa-moon')){
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
-        mailD.classList.toggle('d-lg-none');
-        mailL.classList.toggle('d-lg-inline');
-        gabrielD.classList.toggle('d-sm-none');
-        gabrielL.classList.toggle('d-sm-inline');
-        notebookD.classList.toggle('d-md-none');
-        notebookL.classList.toggle('d-md-inline');
+        mailL.classList.toggle('d-lg-none');
+        mailD.classList.toggle('d-lg-inline');
+        gabrielL.classList.toggle('d-sm-none');
+        gabrielD.classList.toggle('d-sm-inline');
+        notebookL.classList.toggle('d-md-none');
+        notebookD.classList.toggle('d-md-inline');
 
         return;
     }
-        mode.classList.add('fa-moon');
-        mode.classList.remove('fa-sun');
-        mailD.classList.toggle('d-lg-none');
-        mailL.classList.toggle('d-lg-inline');
-        gabrielD.classList.toggle('d-sm-none');
-        gabrielL.classList.toggle('d-sm-inline');
-        notebookD.classList.toggle('d-md-none');
-        notebookL.classList.toggle('d-md-inline');
+        mode.classList.add('fa-sun');
+        mode.classList.remove('fa-moon');
+        mailL.classList.toggle('d-lg-none');
+        mailD.classList.toggle('d-lg-inline');
+        gabrielL.classList.toggle('d-sm-none');
+        gabrielD.classList.toggle('d-sm-inline');
+        notebookL.classList.toggle('d-md-none');
+        notebookD.classList.toggle('d-md-inline');
     }   
 );
 
